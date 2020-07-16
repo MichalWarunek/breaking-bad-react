85333f28d9071ea0c4aa61e1ce5353db359004e0
MichalWarunek <michal.warunek@gmail.com>
2020-07-16 12:56:28 +0200

    Header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
-
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 
 export default class Header extends Component {
 
 
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <AppBar style={{backgroundColor: "black" , borderBottom: "1px solid white"}}>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Button color="inherit" style={{marginLeft: 'auto'}}>
+                        BREAKING BAD API
+                    </Button>
+                </Toolbar>
+            </AppBar>
         );
     }
 
